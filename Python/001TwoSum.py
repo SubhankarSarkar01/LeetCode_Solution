2d10c548faee8a5b587bbb5c72ec7f6ff426fa78
SubhankarSarkar01 <subhankarsarkar50069@gmail.com>
2025-07-04 20:47:57 +0530

    Two-Sum Solution

---
+++ Python/001TwoSum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # -- Here Used unordered_map -- 
+        num_map = {}
+        for i, first in enumerate(nums):
+            sec = target - first
+            if sec in num_map:
+                return [num_map[sec],i]
+
+            num_map[first] = i
+        return []
