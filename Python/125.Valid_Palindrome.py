feee250592ca42941824cb4cc109e93006bd4d23
SubhankarSarkar01 <subhankarsarkar50069@gmail.com>
2025-07-08 19:52:52 +0530

    Valid Palindrome solve

---
+++ Python/125.Valid_Palindrome.py
@@ -0,0 +1,23 @@
+# -- 125. Valid Palindrome-- 
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st, end  = 0, len(s)-1
+        while(st < end):
+            if not s[st].isalnum():
+                st +=1
+                continue
+            elif not s[end].isalnum():
+                end -=1
+                continue
+            elif s[st].lower() != s[end].lower():
+                return False
+            
+            st +=1
+            end -=1
+
+        return True
+    
+# Time Complexity : O(n)
+            
+        
